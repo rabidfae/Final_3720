cfc8c10f6ad2b03e92b94a502bbba986cd14622c
Laura <111711876+rabidfae@users.noreply.github.com>
2024-11-04 11:32:18 -0700

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    './components/**/*.{html,js}',
+    './script.js',
+    './index.html',
+    './server.js',],
+  theme: {
+    extend: {  
+      colors:{
+      primary : { 
+        light: 'rgb(161,229,195)',
+       dark: 'rgb(20,154,124)',
+      },
+     
+  },},
+  
+  plugins: [],
+}
+}
+

9c00ee7ce39a3bee1d0785a1a857b9732bbb5309
Laura <111711876+rabidfae@users.noreply.github.com>
2024-11-04 15:50:06 -0700

    added read me

---
+++ README.md
@@ -0,0 +1,3 @@
+to run tailwind: npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+
+go to ./src/index.html
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+
+
+
 app.use(express.json())
 
 app.get('/', (req, res) => {    
